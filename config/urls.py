81696f3c68bd173e37611f40da6bdc506623690f
unnikrishnan803 <your-username@users.noreply.github.com>
2026-02-24 19:05:10 +0530

    Add healthz endpoint

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+def health_check(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/game/", include("apps.game.urls")),
+    path("healthz", health_check),
 ]
