b5672a9f26ed109c6b34af3165bb5e7cbe85b5a6
unnikrishnan803 <your-username@users.noreply.github.com>
2026-02-24 19:19:07 +0530

    Fix ASGI ImproperlyConfigured error

--- config/asgi.py
+++ config/asgi.py
@@ -1,12 +1,14 @@
 import os
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+django.setup()
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 from .routing import websocket_urlpatterns
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
